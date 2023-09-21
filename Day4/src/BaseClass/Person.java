52d87f48e156fd861687b4a79f84e8d8a25a3b51
ASODARIYA MEET GHANSHYAMBHAI <109502146+meetasodariya@users.noreply.github.com>
2023-09-21 22:27:55 +0530

    Add files via upload

---
+++ Day4/src/BaseClass/Person.java
@@ -0,0 +1,44 @@
+package BaseClass;
+
+
+class Person {
+     private String name;
+     private int age;
+     private String gender;
+     private double income;
+     private double tax;
+	public double getTax() {
+		return tax;
+	}
+	public void setTax(double tax) {
+		this.tax = tax;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public double getIncome() {
+		return income;
+	}
+	public void setIncome(double income) {
+		this.income = income;
+	}
+	
+	
+     
+     
+}
